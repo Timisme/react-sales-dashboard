589d9e0be92a6968962d2715b677cdcc67ff9cbc
tim <ggg580905@gmail.com>
2023-05-04 16:49:57 +0200

    add team page

---
+++ src/scenes/team/index.tsx
@@ -0,0 +1,50 @@
+import { Box, Typography, useTheme } from "@mui/material";
+import { DataGrid } from "@mui/x-data-grid";
+import { tokens } from "../../theme";
+import { mockDataTeam } from "../../data/mockData";
+import Header from "../../components/Header";
+import getColumns from "./utils";
+
+const Team = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const columns = getColumns(colors);
+  return (
+    <Box m="20px">
+      <Header title="團隊" subtitle="成員管理" />
+      <Box
+        m="40px 0 0 0"
+        height="75vh"
+        sx={{
+          "& .MuiDataGrid-root": {
+            border: "none"
+          },
+          "& .MuiDataGrid-cell": {
+            borderBottom: "none"
+          },
+          "& .name-column--cell": {
+            color: colors.greenAccent[300]
+          },
+          "& .MuiDataGrid-columnHeaders": {
+            backgroundColor: colors.blueAccent[700],
+            borderBottom: "none"
+          },
+          "& .MuiDataGrid-virtualScroller": {
+            backgroundColor: colors.primary[400]
+          },
+          "& .MuiDataGrid-footerContainer": {
+            borderTop: "none",
+            backgroundColor: colors.blueAccent[700]
+          },
+          "& .MuiCheckbox-root": {
+            color: `${colors.greenAccent[200]} !important`
+          }
+        }}
+      >
+        <DataGrid checkboxSelection rows={mockDataTeam} columns={columns} />
+      </Box>
+    </Box>
+  );
+};
+
+export default Team;
---
+++ src/scenes/team/utils.tsx
@@ -0,0 +1,68 @@
+import AdminPanelSettingsOutlinedIcon from "@mui/icons-material/AdminPanelSettingsOutlined";
+import LockOpenOutlinedIcon from "@mui/icons-material/LockOpenOutlined";
+import SecurityOutlinedIcon from "@mui/icons-material/SecurityOutlined";
+import { Box, Typography } from "@mui/material";
+
+function getColumns(colors: any) {
+  const columns = [
+    { field: "id", headerName: "ID" },
+    {
+      field: "name",
+      headerName: "Name",
+      flex: 1,
+      cellClassName: "name-column--cell"
+    },
+    {
+      field: "age",
+      headerName: "Age",
+      type: "number",
+      headerAlign: "left",
+      align: "left"
+    },
+    {
+      field: "phone",
+      headerName: "Phone Number",
+      flex: 1
+    },
+    {
+      field: "email",
+      headerName: "Email",
+      flex: 1
+    },
+    {
+      field: "accessLevel",
+      headerName: "Access Level",
+      flex: 1,
+      renderCell: ({ row: { access } }) => {
+        return (
+          <Box
+            width="60%"
+            m="0 auto"
+            p="5px"
+            display="flex"
+            justifyContent="center"
+            borderRadius="4px"
+            sx={{
+              backgroundColor:
+                access === "admin"
+                  ? colors.greenAccent[600]
+                  : access === "manager"
+                  ? colors.greenAccent[700]
+                  : colors.greenAccent[700]
+            }}
+          >
+            {access === "admin" && <AdminPanelSettingsOutlinedIcon />}
+            {access === "manager" && <SecurityOutlinedIcon />}
+            {access === "user" && <LockOpenOutlinedIcon />}
+            <Typography color={colors.grey[100]} sx={{ ml: "5px" }}>
+              {access}
+            </Typography>
+          </Box>
+        );
+      }
+    }
+  ];
+  return columns;
+}
+
+export default getColumns;
